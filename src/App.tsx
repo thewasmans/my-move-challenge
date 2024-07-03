1cd4817c9866255c6c6d2bda996febfabf9e2f9c
thewasmans <96111319+thewasmans@users.noreply.github.com>
2024-07-03 15:26:43 +0200

    Create quick global app layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,42 @@ import './App.css'
 function App()
 {
   return (
-    <div> App MyMove Technical Test </div>
+    <div>
+      <div>
+        <div>
+          <span>Find</span>
+          <input placeholder='Type an adress'/>
+          <button> Search </button>
+        </div>
+        <div>
+          <div><button> Food A </button> <button> Food B </button> <button> Food C </button></div>
+        </div>
+        <div> <input placeholder='Filter critere'/> </div>
+        <div>
+          <div>
+            <h3> Restaurant A</h3>
+            <img  src ='./' />
+            <div>
+              <span> Address</span>
+              <span> Phone</span>
+              <span> Website</span>
+              <span> Schedule</span>
+            </div>
+          </div>
+          <div>
+            <h3> Restaurant B</h3>
+            <img  src ='./' />
+            <div>
+              <span> Address</span>
+              <span> Phone</span>
+              <span> Website</span>
+              <span> Schedule</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div> Restaurant Card Component </div>
+    </div>
   )
 }
 
