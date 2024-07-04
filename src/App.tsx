8f385380958bd29e8fc52490828668d63f62093c
thewasmans <96111319+thewasmans@users.noreply.github.com>
2024-07-04 10:08:08 +0200

    Add and use dark theme for shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { useState } from 'react';
 import IRestaurant from './interfaces/IRestaurant';
 import PageRestaurantFinder from './pages/PageRestaurantFinder'
 import { RestaurantCard } from './components/RestaurantCard';
+import { ThemeProvider } from './components/theme-provider';
 
 function App()
 {
   const [restaurantSelected, setRestaurantSelected] = useState(undefined as IRestaurant | undefined);
 
   return (
-    <div>
-      {restaurantSelected && <RestaurantCard restaurant={restaurantSelected} />}
-      <PageRestaurantFinder cardClicked={r => setRestaurantSelected(r)} onNewSearch={() => setRestaurantSelected(undefined)}/>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex flex-col items-center justify-center h-screen">
+        {restaurantSelected && <RestaurantCard restaurant={restaurantSelected} />}
+        <PageRestaurantFinder cardClicked={r => setRestaurantSelected(r)} onNewSearch={() => setRestaurantSelected(undefined)}/>
+      </div>
+    </ThemeProvider>
   )
 }
 
